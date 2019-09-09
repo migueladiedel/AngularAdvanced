fb3f02b6ae10ad5dfca58d56fde87f85a4ae3f46
Miguel Ángel <maddgm92@gmail.comm>
2019-09-10 00:29:15 +0200

    Register añadido a las rutas y al app.module, cambio de ruta del ya tienes cuenta al /login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { APP_ROUTES } from './app.routes';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './login/register.component';
+
 import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { ProgressComponent } from './pages/progress/progress.component';
@@ -16,10 +18,13 @@ import { SidebarComponent } from './shared/sidebar/sidebar.component';
 import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
 import { PagesComponent } from './pages/pages.component';
 
+
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
+    RegisterComponent,
+    
     NopagefoundComponent,
     DashboardComponent,
     ProgressComponent,
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,14 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { PagesComponent } from './pages/pages.component';
 
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './login/register.component';
 import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
+
 import { ProgressComponent } from './pages/progress/progress.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { LoginComponent } from './login/login.component';
 import { Graficas1Component } from './pages/graficas1/graficas1.component';
+
 
 
 
@@ -22,6 +25,7 @@ const appRoutes: Routes = [
         ]
     },
     { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
     { path: '**', component: NopagefoundComponent }
 ];
 
